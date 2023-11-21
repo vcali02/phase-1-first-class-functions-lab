808892713ca7c8c4cb77d6bc5d507a782a21f233
Val <122405969+vcali02@users.noreply.github.com>
2023-11-21 08:56:00 -0700

    first two complete

--- index.js
+++ index.js
@@ -1,29 +1,84 @@
+//DRIVERS ARRAY
+
+const driversArr = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+//one argument: array of names of drivers
+//return a new array with the 1st two drivers in the array
+
+const returnFirstTwoDrivers = function (driversArr){
+    return driversArr.slice(0, 2)
+}
+
+console.log(returnFirstTwoDrivers(driversArr))
+
+
+//one argument: array of names of drivers
+//return a new array with the last two drivers in the array
+
+const returnLastTwoDrivers = function(driversArr){
+    return driversArr.slice(-2)
+}
+console.log(returnLastTwoDrivers(driversArr))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Code your solution in this file!
-const returnFirstTwoDrivers = function (drivers) {
-    return drivers.slice(0, 2);
-}
+// const returnFirstTwoDrivers = function (drivers) {
+//     return drivers.slice(0, 2);
+// }
 
-const returnLastTwoDrivers = function (drivers) {
-    return drivers.slice(-2);
-}
+// const returnLastTwoDrivers = function (drivers) {
+//     return drivers.slice(-2);
+// }
 
-const selectingDrivers = [
-    returnFirstTwoDrivers,
-    returnLastTwoDrivers,
-]
+// const selectingDrivers = [
+//     returnFirstTwoDrivers,
+//     returnLastTwoDrivers,
+// ]
 
-const createFareMultiplier = function (fare) {
-    return function(multiplier) {return fare * multiplier;
+// const createFareMultiplier = function (fare) {
+//     return function(multiplier) {return fare * multiplier;
 
-    };
-} 
- console.log(createFareMultiplier);
+//     };
+// } 
+//  console.log(createFareMultiplier);
 
-const fareDoubler = createFareMultiplier(2) 
+// const fareDoubler = createFareMultiplier(2) 
 
-const fareTripler = createFareMultiplier(3)
+// const fareTripler = createFareMultiplier(3)
 
-const selectDifferentDrivers = ( drivers, someFunction) => someFunction(drivers)
+// const selectDifferentDrivers = ( drivers, someFunction) => someFunction(drivers)
 
 /*`selectDifferentDrivers()` — This function takes two arguments, an array of
   the names of Scuber's `drivers` and either the `returnFirstTwoDrivers()` or

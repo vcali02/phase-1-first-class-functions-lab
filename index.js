7765280fcc4c5c9a69842e03d031b53729154ca1
Val <122405969+vcali02@users.noreply.github.com>
2023-11-21 09:53:07 -0700

    complete

--- index.js
+++ index.js
@@ -20,16 +20,38 @@ const returnLastTwoDrivers = function(driversArr){
 }
 console.log(returnLastTwoDrivers(driversArr))
 
+//selectingDrivers = [returnFirstTwoDrivers(), returnLastTwoDrivers()]
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
 
 
+//createFareMultiplier() = higher-order function that takes in one argument, integer
+//returns a function that MULTIPLIES a fare ride
+
+const createFareMultiplier = function (fare) {
+    return function(multiplier){
+        return fare * multiplier
+    }
+}
 
 
+const fareDoubler = createFareMultiplier(2)
+console.log(fareDoubler(2))
+
+const fareTripler = createFareMultiplier(3)
+console.log(fareTripler(3))
+
+//selectDifferentDrivers() takes two arguments
+//      an array of the names of Scuber's drivers
+//      and either returnFirstTwoDrivers() or returnLastTwoDrivers() 
+
+function selectDifferentDrivers(drivers, aFunction){
+    //the below is just the "dress rehersal" / instruction 
+    //for what to do with the parameters once they're passed into the function
+    return aFunction(drivers)
+}
 
 
-
-
-
-
+console.log(selectDifferentDrivers(driversArr, returnLastTwoDrivers))
 
 
 
@@ -68,8 +90,8 @@ console.log(returnLastTwoDrivers(driversArr))
 // ]
 
 // const createFareMultiplier = function (fare) {
-//     return function(multiplier) {return fare * multiplier;
-
+//     return function(multiplier) {
+//          return fare * multiplier;
 //     };
 // } 
 //  console.log(createFareMultiplier);
